e6c95277a588eb92065a584a237271a2df38b73c
이은지 <oieunxe@gmail.com>
2026-07-09 14:16:02 +0900

    Initial Commit

---
+++ NOMA/NOMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOMAApp.swift
+//  NOMA
+//
+//  Created by 이은지 on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct NOMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
