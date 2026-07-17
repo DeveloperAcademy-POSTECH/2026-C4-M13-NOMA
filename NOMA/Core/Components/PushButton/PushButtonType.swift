d26b1dfb323c9daff854d2c330eca55354ff6486
이은지 <oieunxe@gmail.com>
2026-07-18 00:05:06 +0900

    style: #11 버튼 상태 당 title, background, overlay 컬러 정의

--- NOMA/Core/Components/PushButton/PushButtonType.swift
+++ NOMA/Core/Components/PushButton/PushButtonType.swift
@@ -5,4 +5,39 @@
 //  Created by 이은지 on 7/17/26.
 //
 
-import Foundation
+import SwiftUI
+
+enum PushButtonType {
+    case neutral
+    case `default`
+    case borderless
+    
+    func titleColor(for status: PushButtonStatus) -> Color {
+        switch (self, status) {
+        case (.neutral, .normal), (.neutral, .hover): return Color(nsColor: .labelColor)
+        case (.neutral, .disabled): return Color(nsColor: .tertiaryLabelColor)
+        case (.default, .normal), (.default, .hover): return .blue
+        case (.default, .disabled): return .blue.opacity(0.35)
+        case (.borderless, .normal), (.borderless, .hover): return .accentColor
+        case (.borderless, .disabled): return .accentColor.opacity(0.35)
+        }
+    }
+    
+    func backgroundColor(for status: PushButtonStatus) -> Color {
+        switch (self, status) {
+        case (.neutral, .normal): return .black.opacity(0.05)
+        case (.neutral, .hover): return .black.opacity(0.15)
+        case (.neutral, .disabled): return .black.opacity(0.05)
+        case (.default, .normal), (.default, .hover), (.default, .disabled): return .blue.opacity(0.1)
+        case (.borderless, .normal): return .clear
+        case (.borderless, .hover), (.borderless, .disabled): return Color(nsColor: .tertiaryLabelColor).opacity(0.12)
+        }
+    }
+    
+    func overlayColor(for status: PushButtonStatus) -> Color? {
+        switch (self, status) {
+        case (.default, .hover): return .black.opacity(0.08)
+        default: return nil
+        }
+    }
+}
