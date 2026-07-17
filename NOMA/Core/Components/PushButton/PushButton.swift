8d8020447d4ad2cb2188730a58e99dc0a7101657
이은지 <oieunxe@gmail.com>
2026-07-18 00:10:14 +0900

    style: #11 PushButton 컴포넌트 구현

--- NOMA/Core/Components/PushButton/PushButton.swift
+++ NOMA/Core/Components/PushButton/PushButton.swift
@@ -5,4 +5,37 @@
 //  Created by 이은지 on 7/17/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct PushButton: View {
+
+    // MARK: - Properties
+
+    private let title: String
+    private let type: PushButtonType
+    private let size: PushButtonSize
+    private let action: () -> Void
+
+    // MARK: - Initializer
+
+    init(
+        title: String,
+        type: PushButtonType,
+        size: PushButtonSize = .medium,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.type = type
+        self.size = size
+        self.action = action
+    }
+
+    // MARK: - Body
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+        }
+        .buttonStyle(PushButtonStyle(type: type, size: size))
+    }
+}
