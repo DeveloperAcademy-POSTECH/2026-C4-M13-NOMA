4604c8cc3a1d807818b68d05b474508c28aab590
이은지 <oieunxe@gmail.com>
2026-07-18 00:03:37 +0900

    style: #11 PushButton horizontalPadding 추가

--- NOMA/Core/Components/PushButton/PushButtonSize.swift
+++ NOMA/Core/Components/PushButton/PushButtonSize.swift
@@ -10,21 +10,28 @@ import SwiftUI
 enum PushButtonSize {
     case small
     case medium
-
+    
     var height: CGFloat {
         switch self {
         case .small: return 24
         case .medium: return 44
         }
     }
-
-    var horizontalPadding: CGFloat {
+    
+    var verticalPadding: CGFloat {
         switch self {
         case .small: return 4
         case .medium: return 13
         }
     }
-
+    
+    var horizontalPadding: CGFloat {
+        switch self {
+        case .small: return 16
+        case .medium: return 16
+        }
+    }
+    
     var font: Font {
         switch self {
         case .small: return .system(size: 13, weight: .medium)
---
+++ NOMA/Core/Components/PushButton/PushButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  PushButtonStyle.swift
+//  NOMA
+//
+//  Created by 이은지 on 7/17/26.
+//
+
+import Foundation
