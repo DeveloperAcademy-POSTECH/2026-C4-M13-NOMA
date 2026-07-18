b98e9b0f9f2833cdff304154460d734001130a51
이은지 <oieunxe@gmail.com>
2026-07-18 15:15:27 +0900

    feat: #16 인터뷰 질문 Model 정의

---
+++ NOMA/Models/InterviewQuestion.swift
@@ -0,0 +1,13 @@
+//
+//  InterviewQuestion.swift
+//  NOMA
+//
+//  Created by 이은지 on 7/18/26.
+//
+
+import Foundation
+
+struct InterviewQuestion {
+    let questionID: String
+    let content: String
+}
