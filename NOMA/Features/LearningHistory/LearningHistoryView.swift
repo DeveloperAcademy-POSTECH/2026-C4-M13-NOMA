164d828a453e0bc67d3b07adeb0e94774f420ce8
이은지 <oieunxe@gmail.com>
2026-07-20 11:00:11 +0900

    style: #28 학습 기록 UI 구현

--- NOMA/Features/LearningHistory/LearningHistoryView.swift
+++ NOMA/Features/LearningHistory/LearningHistoryView.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct LearningHistoryView: View {
+    
+    // MARK: - Properties
+    
+    // FIXME: - 예시 데이터, 모델 배열로 교체할 예정
+    private let sessionNumbers = Array(1...50).reversed()
+
+    private let columns = [
+        GridItem(.fixed(333.5), spacing: 24),
+        GridItem(.fixed(333.5), spacing: 24),
+        GridItem(.fixed(333.5), spacing: 24)
+    ]
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            SectionHeaderView(
+                title: "학습 기록",
+                actionTitle: "홈으로 이동"
+            ) {
+                // FIXME: - 액션 주입
+                print("홈으로 이동 버튼 탭")
+            }
+
+            learningHistoryScrollView
+        }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .top
+        )
+    }
+}
+
+// MARK: - SubViews
+
+extension LearningHistoryView {
+    private var learningHistoryScrollView: some View {
+        ScrollView {
+            learningHistoryCardViewList
+                .padding(.vertical, 40)
+        }
+    }
+    
+    private var learningHistoryCardViewList: some View {
+        LazyVGrid(
+            columns: columns,
+            spacing: 24
+        ) {
+            ForEach(
+                sessionNumbers,
+                id: \.self
+            ) { _ in
+                LearningHistoryCardView()
+            }
+        }
     }
 }
 
