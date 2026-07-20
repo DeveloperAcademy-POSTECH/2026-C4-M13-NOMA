4954ebf8fa2f5369d1ff8f6e7105a06a74d2cc56
이은지 <oieunxe@gmail.com>
2026-07-20 17:21:35 +0900

    style: #27 PracticeSummaryView에 스크립트 블록 컴포넌트 추가

--- NOMA/Features/PracticeSummary/PracticeSummaryView.swift
+++ NOMA/Features/PracticeSummary/PracticeSummaryView.swift
@@ -39,18 +39,26 @@ struct PracticeSummaryView: View {
 
 extension PracticeSummaryView {
     private var practiceSummaryScrollView: some View {
-        VStack(spacing: 0) {
+        VStack(alignment: .leading,spacing: 0) {
             HStack {
                 Text("2026.07.08")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
                     .padding(.trailing, 42)
                 
                 Text("IT • 개발")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
                     .padding(.trailing, 42)
                 
                 Text("자기소개 • 직무 역량")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
                     .padding(.trailing, 42)
 
                 Text("6문항")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
                 
                 Spacer()
                 
@@ -66,6 +74,30 @@ extension PracticeSummaryView {
             
             Divider()
                 .padding(.bottom, 30)
+            
+            // FIXME: - 데이터 주입
+            
+            OverallFeedbackCardView(
+                feedbackText: "전반적으로 격식체 어미가 사용되지 않았습니다.\n다음 학습에서는 '-해요' 대신 '-합니다'를 의식적으로 사용해 보십시오."
+            )
+            .padding(.bottom, 30)
+            
+            Text("스크립트")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundStyle(.primary)
+                .padding(.bottom, 20)
+            
+            QuestionAnswerSectionView(
+                question: "Q1. 자기소개를 해주십시오.",
+                answerText: "안녕하십니까, 저는 지원자 셀리나 입니다.",
+                correction: .init(
+                    originalText: "스페인에서 왔고 한국에서 컴퓨터공학을 전공했어요.",
+                    correctedText: "스페인에서 왔고 한국에서 컴퓨터공학을 전공했습니다.",
+                    explanation: "'-어요'는 비격식체 어미입니다. 이력을 설명할 때는 '-습니다' 체를 사용해야 합니다."
+                )
+            )
+            
         }
     }
 }
