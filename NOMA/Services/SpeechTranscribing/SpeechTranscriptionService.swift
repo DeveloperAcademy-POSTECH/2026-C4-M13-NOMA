6b8c7d60e846579171942fd5a3cadd30dd03fe66
안동선(Andy) <dongsun1115@hotmail.com>
2026-07-20 15:01:08 +0900

    feat: #6 SpeechTranscriptionService 기능 구현

--- NOMA/Services/SpeechTranscribing/SpeechTranscriptionService.swift
+++ NOMA/Services/SpeechTranscribing/SpeechTranscriptionService.swift
@@ -5,21 +5,125 @@
 //  Created by 앤디 on 7/19/26.
 //
 
+import AVFoundation
 import Speech
 
-struct SpeechTranscriptionService {
-    let audioRecorder = AudioRecordingService()
+struct SpeechTranscriptionService: SpeechTranscribing {
+    let audioRecordingService = AudioRecordingService()
     
     let transcriber = SpeechTranscriber(locale: Locale(identifier: "ko-KR"), preset: .progressiveTranscription)
     
-//    func transcribe(bufferStream: AsyncStream<SendableAudioBuffer>)
-//    -> AsyncThrowingStream<TranscriptUpdate, Error> {
-//        
-//    }
-    
-    func requestAssetInstallation() async {
+    func requestAssetInstallation() async throws {
         if let installationRequest = try await AssetInventory.assetInstallationRequest(supporting: [transcriber]) {
             try await installationRequest.downloadAndInstall()
         }
+    }
+    
+    func transcribe(bufferStream: AsyncStream<SendableAudioBuffer>) async throws
+    -> AsyncThrowingStream<TranscriptUpdate, Error> {
+        let (inputSequence, inputContinuation) = AsyncStream.makeStream(of: AnalyzerInput.self)
+        let (outputSequence, outputContinuation) = AsyncThrowingStream.makeStream(of: TranscriptUpdate.self)
+        
+        guard let targetFormat = await SpeechAnalyzer.bestAvailableAudioFormat(compatibleWith: [transcriber]) else {
+            print("모듈에 적합한 포맷 찾을 수 없음")
+            
+            throw AudioError.formatNotFound
+        }
+        
+        let analyzer = SpeechAnalyzer(modules: [transcriber])
+        
+        do {
+            try await analyzer.prepareToAnalyze(in: targetFormat)
+            print("analyzer 준비 완료")
+        } catch {
+            print("analyzer 준비 실패")
+        }
+        
+        Task {
+            do {
+                let recordingStream = try audioRecordingService.startRecording()
+                let sourceFormat = audioRecordingService.audioEngine.inputNode.outputFormat(forBus: 0)
+                
+                for await sendableBuffer in recordingStream {
+                    guard let convertedBuffer = convertFormat(inputBuffer: sendableBuffer, sourceFormat: sourceFormat, targetFormat: targetFormat) else {
+                        print("오디오 포맷 변환 실패")
+                        
+                        continue
+                    }
+                    
+                    let analyzerInput = AnalyzerInput(buffer: convertedBuffer)
+                    inputContinuation.yield(analyzerInput)
+                }
+                
+                inputContinuation.finish()
+            } catch {
+                print("audioRecordingService.startRecording() 실패: \(error)")
+                inputContinuation.finish()
+                outputContinuation.finish(throwing: error)
+            }
+        }
+        
+        Task {
+            do {
+                for try await result in transcriber.results {
+                    let attributedText = result.text
+                    let transcriptUpdate = TranscriptUpdate(text: attributedText, isFinal: false)
+                    
+                    outputContinuation.yield(transcriptUpdate)
+                }
+            } catch {
+                print("STT 변환 실패: \(error)")
+                
+            }
+        }
+        
+        return outputSequence
+    }
+    
+    func convertFormat(
+        inputBuffer: SendableAudioBuffer,
+        sourceFormat: AVAudioFormat,
+        targetFormat: AVAudioFormat
+    ) -> AVAudioPCMBuffer? {
+        guard let audioConverter = AVAudioConverter(from: sourceFormat, to: targetFormat) else {
+            print("AVAudioConverter 생성 실패")
+            
+            return nil
+        }
+        
+        nonisolated(unsafe) let rawBuffer = inputBuffer.pcmBuffer
+        
+        let capacity = AVAudioFrameCount(
+            Double(rawBuffer.frameLength) * (targetFormat.sampleRate / sourceFormat.sampleRate)
+        )
+        
+        guard let outputBuffer = AVAudioPCMBuffer(pcmFormat: targetFormat, frameCapacity: capacity) else {
+            print("outputBuffer 생성 실패")
+            
+            return nil
+        }
+        
+        var error: NSError?
+        
+        
+        let inputBlock: AVAudioConverterInputBlock = { requestedPackets, statusPointer in
+            statusPointer.pointee = .haveData
+            
+            return rawBuffer
+        }
+        
+        audioConverter.convert(to: outputBuffer, error: &error, withInputFrom: inputBlock)
+
+        if let error = error {
+            print("변환 실패: \(error)")
+            
+            return nil
+        }
+        
+        return outputBuffer
+    }
+    
+    enum AudioError: Error {
+        case formatNotFound
     }
 }
