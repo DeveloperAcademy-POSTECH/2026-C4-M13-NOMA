70b9a732764d1cbe343dd52a03dabf31ac692ec8
YangSeokHoR <aodde951@naver.com>
2026-07-21 00:28:57 +0900

    feat/#18 NOMAApp SwiftData 추가

--- NOMA/Application/NOMAApp.swift
+++ NOMA/Application/NOMAApp.swift
@@ -5,6 +5,7 @@
 //  Created by 이은지 on 7/9/26.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,7 @@ struct NOMAApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [PracticeAnswer.self])
+        .modelContainer(for: [InterviewQuestion.self])
     }
 }
