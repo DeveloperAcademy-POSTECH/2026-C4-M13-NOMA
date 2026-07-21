d92578afff5579abe857cde0fe5995f240473dd1
이은지 <oieunxe@gmail.com>
2026-07-21 23:27:48 +0900

    feat: #43 AppRouter 함수 정의

--- NOMA/Application/AppRouter/AppRouter.swift
+++ NOMA/Application/AppRouter/AppRouter.swift
@@ -5,4 +5,27 @@
 //  Created by 이은지 on 7/21/26.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+final class AppRouter {
+    
+    // MARK: - Properties
+    
+    var path = NavigationPath()
+
+    // MARK: - Functions
+    
+    func push(_ route: AppRoute) {
+        path.append(route)
+    }
+
+    func pop() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+}
