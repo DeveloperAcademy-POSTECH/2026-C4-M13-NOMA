b09035c19e5d172fd94baad84499a3e0935cc7b7
이은지 <oieunxe@gmail.com>
2026-07-21 22:22:55 +0900

    style: 로딩 인디케이터 및 alert 구현

--- NOMA/Features/PracticeSummary/AnswerAnalysisLoadingView.swift
+++ NOMA/Features/PracticeSummary/AnswerAnalysisLoadingView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct AnswerAnalysisLoadingView: View {
     
+    // MARK: - Properties
+    
+    @State private var isShowingExitAlert = false
+    
     // MARK: - Body
     
     var body: some View {
+        VStack(spacing: 0) {
+            analyzingStatusView
+        }
+        .alert(
+            "정말 나가시겠습니까?",
+            isPresented: $isShowingExitAlert
+        ) {
+            Button(
+                "나가기",
+                role: .destructive
+            ) {
+                print("종료 확정됨")
+            }
+            Button(
+                "취소",
+                role: .cancel
+            ) {
+            }
+        } message: {
+            Text("지금 화면을 벗어나시면 지금까지 진행된 면접 내용과 설정 정보는 저장되지 않습니다. 그래도 종료하시겠습니까?")
+        }
+    }
+}
+
+// MARK: - SubViews
+
+extension AnswerAnalysisLoadingView {
+    private var analyzingStatusView: some View {
         VStack(spacing: 20) {
             SpinningRingLoader()
                 .frame(width: 32, height: 32)
