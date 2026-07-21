2e592491871317a1598f20c947115def2cc34b96
이은지 <oieunxe@gmail.com>
2026-07-21 23:28:37 +0900

    feat: #43 RootView 정의 및 각 case별 View 연결

--- NOMA/Application/AppRouter/AppRootView.swift
+++ NOMA/Application/AppRouter/AppRootView.swift
@@ -5,4 +5,38 @@
 //  Created by 이은지 on 7/21/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct RootView: View {
+    
+    // MARK: - Properties
+    
+    @State private var router = AppRouter()
+
+    // MARK: - Body
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            OnboardingView()
+                .navigationDestination(for: AppRoute.self) { route in
+                    switch route {
+                    case .onboarding:
+                        OnboardingView()
+                    case .home:
+                        HomeView()
+                    case .permission:
+                        PermissionView()
+                    case .interviewPractice:
+                        InterviewPracticeView()
+                    case .answerAnalysisLoading:
+                        AnswerAnalysisLoadingView()
+                    case .practiceSummary:
+                        PracticeSummaryView()
+                    case .learningHistory:
+                        LearningHistoryView()
+                    }
+                }
+        }
+        .environment(router)
+    }
+}
